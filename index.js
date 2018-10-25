85ab39e51931f715311d3a4ef617424abaa7b7e0
joshuablockchain <44281582+joshuablockchain@users.noreply.github.com>
2018-10-25 17:00:38 +0800

    features

--- index.js
+++ index.js
@@ -6,31 +6,62 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const notesRouter = require('./server/routers/notesRouter');
 const port = 3000;
+const bodyParser = require('body-parser');
+const SimpleJsonStore = require('simple-json-store');
+const indexRouter = require('./server/routers/indexRouter');
 
-app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  req.viewModel = {
-    title: 'Wallet System'
-  };
-  next();
-});
+const store = new SimpleJsonStore('./data.json', {newBalance});
 
-app.use(express.static('public'));
-
-// NOTE: The __dirname is important for setting up the directory of the views
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  let viewModel = req.viewModel;
-  res.render('index.pug', viewModel);
-});
+// Note: The order must be maintained here
+app.use('/', indexRouter);
 
 
-app.use('/api/notes', notesRouter);
+var deposit = 0;
+var withdraw = 0;
+var transfer = 0;
+var payment = 0;
+var newBalance = 0;
+
+app.post('/deposit',(req,res) => {
+  deposit = req.body.deposit
+  newBalance +=  deposit
+  store.set('newBalance', newBalance);
+  res.json(newBalance);
+})
+
+app.post('/withdraw',(req,res) => {
+  withdraw = req.body.withdraw
+  newBalance = newBalance -  withdraw
+  store.set('newBalance', newBalance);
+  res.json(newBalance);
+})
+/////
+app.post('/transfer',(req,res) => {
+  transfer = req.body.transfer
+  newBalance = newBalance -  transfer
+  store.set('newBalance', newBalance);
+  res.json(newBalance);
+})
+//////////
+app.post('/payment',(req,res) => {
+  payment = req.body.payment
+  newBalance = newBalance -  payment
+  store.set('newBalance', newBalance);
+  res.json(newBalance);
+})
+///////////
+
+app.get('/',(req,res)=>{
+  res.send(`${newBalance}`);
+})
+
 
 app.listen(port, (err) => {
   if(err) { return console.error(err); }
