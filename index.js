3a0aab687e3656955fd2454d0e305b071c7c0759
joshuablockchain <44281582+joshuablockchain@users.noreply.github.com>
2018-10-26 15:50:08 +0800

    update-balance

--- index.js
+++ index.js
@@ -9,58 +9,28 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const SimpleJsonStore = require('simple-json-store');
+const walletRouter = require('./server/routers/walletRouter');
 const indexRouter = require('./server/routers/indexRouter');
-
-
-const store = new SimpleJsonStore('./data.json', {newBalance});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
+app.use((req, res, next) => {
+  req.viewModel = {
+    balance: 'Card - Note Taking App'
+  };
+  next();
+});
 
 // Note: The order must be maintained here
+//
+//app.use('/', walletRouter);
 app.use('/', indexRouter);
+// axios.post('/deposit',(req,re)=>{
 
-
-var deposit = 0;
-var withdraw = 0;
-var transfer = 0;
-var payment = 0;
-var newBalance = 0;
-
-app.post('/deposit',(req,res) => {
-  deposit = req.body.deposit
-  newBalance +=  deposit
-  store.set('newBalance', newBalance);
-  res.json(newBalance);
-})
-
-app.post('/withdraw',(req,res) => {
-  withdraw = req.body.withdraw
-  newBalance = newBalance -  withdraw
-  store.set('newBalance', newBalance);
-  res.json(newBalance);
-})
-/////
-app.post('/transfer',(req,res) => {
-  transfer = req.body.transfer
-  newBalance = newBalance -  transfer
-  store.set('newBalance', newBalance);
-  res.json(newBalance);
-})
-//////////
-app.post('/payment',(req,res) => {
-  payment = req.body.payment
-  newBalance = newBalance -  payment
-  store.set('newBalance', newBalance);
-  res.json(newBalance);
-})
-///////////
-
-app.get('/',(req,res)=>{
-  res.send(`${newBalance}`);
-})
+// })
 
 
 app.listen(port, (err) => {
