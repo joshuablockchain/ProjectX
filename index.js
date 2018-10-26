96d4876fcea4d89c670e6e83239e7008af4cd80d
joshuablockchain <44281582+joshuablockchain@users.noreply.github.com>
2018-10-26 18:17:03 +0800

    transaction

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-/**
- * @description
- *wallet system
- */
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
-const SimpleJsonStore = require('simple-json-store');
-const walletRouter = require('./server/routers/walletRouter');
 const indexRouter = require('./server/routers/indexRouter');
+const walletRouter = require('./server/routers/walletRouter');
+const port = 3000;
+
+app.use(morgan('dev'));
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'server/views'));
-app.set('view engine', 'pug');
 app.use((req, res, next) => {
   req.viewModel = {
-    balance: 'Card - Note Taking App'
+    title: 'Wallet System'
   };
   next();
 });
 
-// Note: The order must be maintained here
-//
-//app.use('/', walletRouter);
-app.use('/', indexRouter);
-// axios.post('/deposit',(req,re)=>{
+app.use(express.static('public'));
 
-// })
+// NOTE: The __dirname is important for setting up the directory of the views
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'pug');
+
+app.use('/', walletRouter);
 
 
 app.listen(port, (err) => {
